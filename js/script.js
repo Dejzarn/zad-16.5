8e23df8a10eee0597c6c958c96cec3f73a187915
Andrzej Grabowski <andgrabowski@gmail.com>
2019-02-03 19:31:10 +0100

    Add functioncreateTweet

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ function getQuote() {
         })
         .then(createTweet);
 }
+
+function createTweet(input) {
+    var data = input[0];
+
+    var dataElement = document.createElement('div');
+    dataElement.innerHTML = data.content;
+    var quoteText = dataElement.innerText.trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        document.querySelector('.quote').innerText = quoteText;
+        document.querySelector('.author').innerText = "Author: " + quoteAuthor;
+        document.querySelector('.tweet').setAttribute('href', tweet);
+    }
+}
